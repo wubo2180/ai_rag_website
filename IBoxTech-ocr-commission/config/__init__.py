7365a9f0dd27de4447014a4f597dacfd9d4adf7c
SlayTheS <2899450352@qq.com>
2026-06-01 16:16:30 +0800

    Remove standalone OCR service directories

--- IBoxTech-ocr-commission/config/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-V3配置模块
-"""
-
-from .settings import V3Config, DebugLevel, OutputLevel
-
-__all__ = ['V3Config', 'DebugLevel', 'OutputLevel']
--- IBoxTech-ocr-commission/visualization/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-V3可视化模块 - 业务逻辑与可视化完全分离
-"""
-
-from .base_visualizer import BaseVisualizer
-from .step_visualizers import PreprocessingVisualizer
-
-__all__ = ['BaseVisualizer', 'PreprocessingVisualizer']
